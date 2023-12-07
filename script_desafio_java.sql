f02a9b2fd664aeb2468923e886607db0ab3ae830
Caio Arnoni <125586491+caioarnonii@users.noreply.github.com>
2023-12-07 14:38:58 -0300

    updt script

--- script_desafio_java.sql
+++ script_desafio_java.sql
@@ -2,15 +2,17 @@ CREATE DATABASE IF NOT EXISTS desafioJava;
 USE desafioJava;
 
 CREATE TABLE IF NOT EXISTS Usuario (
-  idUsuario INT NOT NULL,
+  idUsuario INT AUTO_INCREMENT,
   nome VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
   senha VARCHAR(100) NOT NULL,
   PRIMARY KEY (idUsuario)
   ) AUTO_INCREMENT = 1;
-
+  
+  SELECT * FROM Usuario;
+    
 CREATE TABLE IF NOT EXISTS Computador (
-  idComputador INT NOT NULL,
+  idComputador INT NOT NULL AUTO_INCREMENT,
   fkUsuario INT NOT NULL,
   ip VARCHAR(100) NOT NULL,
   sistemaOperacional VARCHAR(45) NOT NULL,
@@ -28,7 +30,7 @@ CREATE TABLE IF NOT EXISTS UnidadeMedida (
 
 
 CREATE TABLE IF NOT EXISTS Componente (
-  idComponente INT NOT NULL,
+  idComponente INT NOT NULL AUTO_INCREMENT,
   fkUnidadeMedida INT NOT NULL,
   nomeComponente VARCHAR(45) NOT NULL,
   PRIMARY KEY (idComponente, fkUnidadeMedida),
@@ -38,14 +40,14 @@ CREATE TABLE IF NOT EXISTS Componente (
 CREATE TABLE IF NOT EXISTS ComputadorComponente (
   fkComputador INT NOT NULL,
   fkComponente INT NOT NULL,
-  idComputadorComponente INT NOT NULL,
+  idComputadorComponente INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (idComputadorComponente),
   FOREIGN KEY (fkComputador) REFERENCES Computador (idComputador),
   FOREIGN KEY (fkComponente) REFERENCES Componente (idComponente)
 ) AUTO_INCREMENT = 1;
 
 CREATE TABLE IF NOT EXISTS Registro (
-  idRegistro INT NOT NULL,
+  idRegistro INT NOT NULL AUTO_INCREMENT,
   dadoRegistro VARCHAR(100),
   dataHora DATETIME,
   fkComputadorComponente INT NOT NULL,
@@ -54,7 +56,7 @@ CREATE TABLE IF NOT EXISTS Registro (
 ) AUTO_INCREMENT = 1;
 
 CREATE TABLE IF NOT EXISTS UnidadeMedida (
-  idUnidadeMedida INT NOT NULL,
+  idUnidadeMedida INT NOT NULL AUTO_INCREMENT,
   tipoMedida VARCHAR(45),
   PRIMARY KEY (idUnidadeMedida)
 ) AUTO_INCREMENT = 1;
@@ -64,6 +66,8 @@ CREATE USER IF NOT EXISTS 'JavaUser'@'%' IDENTIFIED BY 'Java2@23';
 GRANT ALL PRIVILEGES ON desafioJava.* TO 'JavaUser'@'%';
 
 FLUSH PRIVILEGES;
+
+
 
 
 
